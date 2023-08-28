47c159037df252ecc7ad0948f6a4b08e077aefd9
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-08-28 09:03:21 +0200

    Creación clase para combate

---
+++ MyNewPokedex/VsMode/VsModeGame.swift
@@ -0,0 +1,43 @@
+//
+//  VsModeGame.swift
+//  MyNewPokedex
+//
+//  Created by Pablo Márquez Marín on 27/8/23.
+//
+
+import Foundation
+
+class Player {
+    var data: Pokemon
+    var hp: Int
+    var atk: Int
+    var def:Int
+    
+    init(data: Pokemon, hp: Int, atk: Int, def: Int) {
+        self.data = data
+        self.hp = data.stats[0].base_stat
+        self.atk = data.stats[1].base_stat
+        self.def = data.stats[2].base_stat
+    }
+}
+
+class VsModeGame {
+    var player1: Player
+    var player2: Player
+    
+    init(player1: Player, player2: Player) {
+        self.player1 = player1
+        self.player2 = player2
+    }
+    
+    func atack(p1: Player, p2: Player) -> Int {
+        var damage = p1.atk - p2.def
+        print(damage)
+        if damage <= 0 {
+            damage = 0
+        }
+        var realDamage = p2.hp - damage
+        
+        return realDamage
+    }
+}
