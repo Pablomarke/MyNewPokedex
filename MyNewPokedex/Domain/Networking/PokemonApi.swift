c4f55b5e6e877d601e049c502e73c659212e1af4
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-12-12 14:00:55 +0100

    Enums in PokemonApi

--- MyNewPokedex/Domain/Networking/PokemonApi.swift
+++ MyNewPokedex/Domain/Networking/PokemonApi.swift
@@ -12,26 +12,46 @@ class PokemonApi {
     // MARK: Constants
     private let baseUrl:String = "https://pokeapi.co/api/v2/"
     static let shared = PokemonApi()
-    private let statusOk = 200...299
+    
+    enum Status {
+        static let ok = 200
+    }
+    
+    enum Endpoint {
+        static let baseUrl = "https://pokeapi.co/api/v2/"
+        static let pokemon = "pokemon/"
+    }
+    
+    enum HttpMethod {
+        static let get = "GET"
+    }
     
     // MARK: Functions
     
     /// Pokemon by ID
-    func getPokemonId(id: Int,  completion: @escaping (Pokemon) -> Void){
-        let idUrl = baseUrl + "pokemon/\(id)/"
+    func getPokemonId(id: Int,  
+                      completion: @escaping (Pokemon) -> Void){
+        let idUrl = Endpoint.baseUrl + Endpoint.pokemon + "\(id)/"
         guard let url = URL(string: idUrl) else {return}
         var urlRequest = URLRequest(url: url )
-        urlRequest.httpMethod = "GET"
+        urlRequest.httpMethod = HttpMethod.get
         
         URLSession.shared.dataTask(with: urlRequest) {data,response,error in
             DispatchQueue.main.async {
-            guard error == nil else { return }
-            guard let data,
-                    (response as? HTTPURLResponse)?.statusCode == 200 else {
-                            return
-                        }
-            guard let pokemon = try? JSONDecoder().decode(Pokemon.self, from: data) else {return}
-            
+                guard error == nil else {
+                    return
+                }
+                
+                guard let data,
+                      (response as? HTTPURLResponse)?.statusCode == Status.ok else {
+                    return
+                }
+                
+                guard let pokemon = try? JSONDecoder().decode(Pokemon.self,
+                                                              from: data) else {
+                    return
+                }
+                
                 completion(pokemon)
             }
         }.resume()
